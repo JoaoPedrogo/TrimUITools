505d79acf2e6adde7c017594f58ebdf35509d58d
João Pedro Gonçalves <71751485+JoaoPedrogo@users.noreply.github.com>
2024-06-13 00:53:05 -0300

    Update LED.sh

--- Tools/LED_Color/LED.sh
+++ Tools/LED_Color/LED.sh
@@ -2,23 +2,25 @@
 
 # Função para alternar as cores dos LEDs entre branco e azul
 set_alternating_led_colors() {
+    # Configurar para branco
     echo 1 > /sys/class/led_anim/effect_enable 
-    echo "FFFFFF" > /sys/class/led_anim/effect_rgb_hex_lr
+    echo "FFFFFF " > /sys/class/led_anim/effect_rgb_hex_lr
     echo 1 > /sys/class/led_anim/effect_cycles_lr
-    echo 10000 > /sys/class/led_anim/effect_duration_lr
-    echo 1 >  /sys/class/led_anim/effect_lr
-    sleep 10
+    echo 2000 > /sys/class/led_anim/effect_duration_lr
+    echo 1 > /sys/class/led_anim/effect_lr
+    sleep 2
     
+    # Configurar para azul
     echo 1 > /sys/class/led_anim/effect_enable 
-    echo "0000FF" > /sys/class/led_anim/effect_rgb_hex_lr
+    echo "0000FF " > /sys/class/led_anim/effect_rgb_hex_lr
     echo 1 > /sys/class/led_anim/effect_cycles_lr
-    echo 10000 > /sys/class/led_anim/effect_duration_lr
-    echo 1 >  /sys/class/led_anim/effect_lr
-    sleep 10
+    echo 2000 > /sys/class/led_anim/effect_duration_lr
+    echo 1 > /sys/class/led_anim/effect_lr
+    sleep 2
 }
 
 # Loop principal
 while true; do
     set_alternating_led_colors
-    sleep 5
+    sleep 1
 done
